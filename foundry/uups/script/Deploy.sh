a0e0e2fd4693c53840278b8ee5c4852f301ff24d
Dongri Jin <dongrium@gmail.com>
2024-06-17 19:20:36 +0900

    Revert "Add uups"
    This reverts commit 0e60b1dd5ce81a28fd9bb439cb1294d6cce728b4.

--- foundry/uups/script/Deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source .env
-forge script script/Deploy.s.sol --rpc-url $SEPOLIA_RPC_URL --broadcast --legacy
--- foundry/uups/script/Upgrade.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source .env
-forge script script/Upgrade.s.sol --rpc-url $SEPOLIA_RPC_URL --broadcast --legacy
